452dc28cf121c05f9532dbf77543d9024739f6a3
phucha1986 <hahanhphuc1986@gmail.com>
2021-07-15 21:23:52 +0700

    loading button

--- ek/src/components/commons/EKButton.js
+++ ek/src/components/commons/EKButton.js
@@ -2,7 +2,17 @@ const EKButton = (params) => {
 
   return (
     <div className="login-form__action-btn">
-      <button id={params.id} aria-disabled="false" className="call-to-action call-to-action__primary call-to-action--middle" onClick={params.handleSubmit}>{params.title}</button>
+      <button id={params.id} aria-disabled="false" className="call-to-action call-to-action__primary call-to-action--middle" onClick={params.handleSubmit}>
+        {params.IsLoading && 
+          <div className="login-form__button-content">
+            <div role="region" aria-label="The information is loading" className="ek-spinner ek-spinner__xsmall login-form__button-loader"></div>
+            <p className="login-form__loading-label" aria-live="off">Loading</p>
+          </div>
+        }
+        {!params.IsLoading && params.title}
+      </button>
+
+      
     </div>   
   );
 }
--- ek/src/components/login/Login.js
+++ ek/src/components/login/Login.js
@@ -12,6 +12,7 @@ function Login() {
   const[errorList, setErrorList] = useState([]);
   const[userNameValue, setUserNameValue] = useState('EK619931045');
   const[passwordValue, setPasswordValue] = useState('test');
+  const[isLogging, setIsLogging] = useState(false);
   
   useEffect(() => {
     document.title = "Login to Emirates";
@@ -22,8 +23,8 @@ function Login() {
 
   const loggedInThunk = (userNameValue) => dispatch => setTimeout(function(){
     if(userNameValue === "EK619931045")
-    {
-      dispatch(loggedInAction());
+    {      
+      dispatch(loggedInAction());      
       localStorage.setItem ('loggedIn', true);
       history.push('/MyStatement');
     }
@@ -40,13 +41,11 @@ function Login() {
     if(passwordValue === '')
     {      
       setErrorList(errorMessages => [...errorMessages, ["Password: This is a mandatory field, please check and try again. "]]);
-    }    
-    //if(userNameValue === "EK619931045") {
-
-      store.dispatch(
-        loggedInThunk(userNameValue)
-      );          
-    //}
+    }        
+    setIsLogging(true);
+    store.dispatch(
+      loggedInThunk(userNameValue)
+    );    
   }
   function handleSubmit(e)
   {    
@@ -75,7 +74,7 @@ function Login() {
                 <div className="login-form__remember">
                   <EKCheckbox id="chkRememberMe" title="Keep me logged in on this device" />
                 </div>
-                <EKButton id="btnLogin" title="Login" handleSubmit={handleSubmit} />
+                <EKButton id="btnLogin" title="Login" handleSubmit={handleSubmit} IsLoading={isLogging} />
                 
               </section></div><div className="split__or-line login-central__or-line"><img title="" alt="" className="split__or login-central__or" aria-hidden="false" src="https://c.ekstatic.net/account/uiassets/or.svg"/></div><div className="split__right login-central__split-right"><div><div className="skywards-form"><section className="skywards-form__second-part-container"><h2 className="skywards-form__title">Not an Emirates Skywards member yet?</h2><div><div className="skywards-form__text"><p>Register now to make the most of every mile with <a data-id="pagebody_link" data-link="Emirates Skywards" title="Emirates Skywards" href="/account/vn/vietnamese/light-registration/?section=MYA&amp;th=8dc2ce9d13387344e461cf5bbacb6861f7c4731d" className="link link--with-hover-bg link--default link--underline skywards-form__link"><span className="link__text link__text--underline">Emirates Skywards</span></a></p></div></div><a data-id="pagebody_cta" data-link="Register now" title="Tham gia ngay" href="/account/vn/vietnamese/light-registration/?section=MYA&amp;th=8dc2ce9d13387344e461cf5bbacb6861f7c4731d&amp;refurl=%2Fvn%2Fvietnamese" className="link link--default skywards-form__register-now call-to-action call-to-action__secondary call-to-action--middle"><span className="link__text">Join now</span></a></section></div></div></div></div></div></div></div>
 
